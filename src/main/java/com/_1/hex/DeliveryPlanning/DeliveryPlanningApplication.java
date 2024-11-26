ea0d53818bc8984ac7ec77bded5a1e884fc05415
Rafael Sagues Rivera <rsaguesriv@insa-lyon.fr>
2024-11-26 11:33:57 +0100

    Initial commit

---
+++ src/main/java/com/_1/hex/DeliveryPlanning/DeliveryPlanningApplication.java
@@ -0,0 +1,13 @@
+package com._1.hex.DeliveryPlanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryPlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryPlanningApplication.class, args);
+	}
+
+}
