e2fc97ec071e9bd2e25c0dce977f311f63d104e4
Mateo Carvajal <mcarvajal@insa-lyon.fr>
2024-11-29 10:08:44 +0100

    added ddelivery and request classes

---
+++ src/main/java/com/_1/hex/DeliveryPlanning/model/Request.java
@@ -0,0 +1,14 @@
+package com._1.hex.DeliveryPlanning.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Request {
+    List<Delivery> trip;
+
+    public Request() {
+        trip = new ArrayList<Delivery>();
+    }
+
+    public List<Delivery> getTrip() {return trip; }
+}
