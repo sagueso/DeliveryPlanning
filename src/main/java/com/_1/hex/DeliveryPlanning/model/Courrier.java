922a20dcd78ab4a557f73834459e1a3bd510299b
rsaguesriv <rafasagues@gmail.com>
2024-11-29 09:36:31 +0100

    Added Courrier

---
+++ src/main/java/com/_1/hex/DeliveryPlanning/model/Courrier.java
@@ -0,0 +1,16 @@
+package com._1.hex.DeliveryPlanning.model;
+
+public class Courrier {
+    private String name;
+    //private Delivery delivery;
+
+    public Courrier(String name) {
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
