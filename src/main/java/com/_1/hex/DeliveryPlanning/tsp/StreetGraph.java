2c6437c03419f89d8a12ba5dc1474f7c420ed8b6
Rafael Sagues <rafasagues@gmail.com>
2024-12-03 15:16:39 +0100

    Added StreetGraph class

---
+++ src/main/java/com/_1/hex/DeliveryPlanning/tsp/StreetGraph.java
@@ -0,0 +1,25 @@
+package com._1.hex.DeliveryPlanning.tsp;
+
+public class StreetGraph implements Graph{
+
+
+    @Override
+    public int getNbVertices() {
+        return 0;
+    }
+
+    @Override
+    public int getCost(int i, int j) {
+        return 0;
+    }
+
+    @Override
+    public boolean isArc(int i, int j) {
+        return false;
+    }
+
+    @Override
+    public Integer getPredecessor(int i) {
+        return 0;
+    }
+}
