76fa05dbe8612a79f7f57f6b0d7e3da962e54470
ber-god <bernardowiz65@gmail.com>
2024-12-03 11:23:16 +0100

    Draw Points is working

--- src/main/java/com/_1/hex/DeliveryPlanning/view/DrawMap.java
+++ src/main/java/com/_1/hex/DeliveryPlanning/view/DrawMap.java
@@ -2,6 +2,7 @@ package com._1.hex.DeliveryPlanning.view;
 
 
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -40,7 +41,7 @@ public class DrawMap extends JFrame {
 
     public DrawMap() {
         super("Map");
-        setSize(480, 200);
+        setSize(1000, 1000);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
     }
@@ -48,6 +49,7 @@ public class DrawMap extends JFrame {
     void defineStreetMap (StreetMap streetMap) {
         this.streetMap = streetMap;
     }
+
     void defineRoute (List<Integer> route) {this.route = route;}
 
     void findMinMax(Map<Integer, Intersection> intersections) {
@@ -74,39 +76,77 @@ public class DrawMap extends JFrame {
 
         List<Street> l = streetMap.getStreets();
         Map<Integer, Intersection> m = streetMap.getIntersections();
-        findMinMax(m);
-        Integer zoom = 1000;
+        //findMinMax(m);
+        Double x1, y1, x2, y2;
 
         for (Street line : l) {
             //System.out.println((line.getOrigin().getLatitude()-45)*100);
-            g3d.draw(new Line2D.Double((line.getOrigin().getLatitude()-minlat)*zoom/(maxlat-minlat), (line.getOrigin().getLongitude()-minlon)*zoom/(maxlon-minlon), (line.getDestination().getLatitude()-minlat)*zoom/(maxlat-minlat), (line.getDestination().getLongitude()-minlon)*zoom/(maxlon-minlon)));
+            x1 = transfLatitude(line.getOrigin().getLatitude());
+            y1 = transfLongitude(line.getOrigin().getLongitude());
+            x2 = transfLatitude(line.getDestination().getLatitude());
+            y2 = transfLongitude(line.getDestination().getLongitude());
+            g3d.draw(new Line2D.Double(x1, y1, x2, y2));
         }
 
     }
 
+    Double transfLatitude(Double latitude){
+        return (latitude - minlat) * 1000 /(maxlat-minlat);
+    }
+    Double transfLongitude(Double longitude){
+        return (longitude - minlon) * 1000 /(maxlon-minlon);
+    }
+/*
     void drawPoints(Graphics g, Intersection point){
         Graphics2D g2d = (Graphics2D) g;
         DrawPoints drawPoints = new DrawPoints();
         drawPoints.drawPoints(g2d, point);
     }
+*/
+
+void drawPoints(Graphics g,Intersection point){
+    Graphics2D graph = (Graphics2D) g;
+
+    Double latitude = point.getLatitude();
+    Double longitude = point.getLongitude();
+
+    Double latTrasf = transfLatitude(latitude);
+    Double lonTrasf = transfLongitude(longitude);
+
+    System.out.println("Latitude: " + latitude + " Longitude: " + longitude);
+
+    System.out.println("Latitude: " + latTrasf + " Longitude: " + lonTrasf);
+
+    graph.setColor(Color.RED);
+    Ellipse2D.Double circle = new Ellipse2D.Double(latTrasf - 5.0, lonTrasf - 5.0, 10, 10);
+    graph.draw(circle);
+    graph.fill(circle);    
+}
 
     void drawRoutes (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         Integer zoom = 1000;
         g2d.setColor(Color.GREEN);
         g2d.setStroke(new BasicStroke(2f));
+        Double x1, y1, x2, y2;
         for (int i=0; i<route.size()-1; i++){
             Integer id1 = route.get(i);
             Integer id2 = route.get(i+1);
             Intersection source = streetMap.getIntersectionById(id1);
             Intersection destination = streetMap.getIntersectionById(id2);
-            g2d.draw(new Line2D.Double((source.getLatitude()-minlat)*zoom/(maxlat-minlat), (source.getLongitude()-minlon)*zoom/(maxlon-minlon), (destination.getLatitude()-minlat)*zoom/(maxlat-minlat), (destination.getLongitude()-minlon)*zoom/(maxlon-minlon)));
+            x1 = transfLatitude(source.getLatitude());
+            y1 = transfLongitude(source.getLongitude());
+            x2 = transfLatitude(destination.getLatitude());
+            y2 = transfLongitude(destination.getLongitude());
+            g2d.draw(new Line2D.Double(x1, y1, x2, y2));
         }
     }
 
 
     public void paint(Graphics g) {
         super.paint(g);
+
+        findMinMax(streetMap.getIntersections());
 
         readLines (g);
         if(route != null) {
--- src/main/java/com/_1/hex/DeliveryPlanning/view/DrawPoints.java
+++ src/main/java/com/_1/hex/DeliveryPlanning/view/DrawPoints.java
@@ -4,6 +4,7 @@ package com._1.hex.DeliveryPlanning.view;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 
 import com._1.hex.DeliveryPlanning.model.Intersection;
@@ -11,17 +12,25 @@ import com._1.hex.DeliveryPlanning.view.DrawPoints;
 
 
 public class DrawPoints {
+
+    DrawMap drawMap = new DrawMap();
+
     void drawPoints(Graphics g,Intersection point){
         Graphics2D graph = (Graphics2D) g;
 
         Double latitude = point.getLatitude();
         Double longitude = point.getLongitude();
 
-        System.out.println("Latitude: " + latitude);
-        System.out.println("Longitude: " + longitude);
+        Double latTrasf = drawMap.transfLatitude(latitude);
+        Double lonTrasf = drawMap.transfLongitude(longitude);
+
+        System.out.println("Latitude: " + latitude + " Longitude: " + longitude);
+
+        System.out.println("Latitude: " + latTrasf + " Longitude: " + lonTrasf);
 
         graph.setColor(Color.RED);
-        graph.drawOval(latitude.intValue(), longitude.intValue(), 5, 5);
-        graph.fillOval(latitude.intValue(), longitude.intValue()*100, 5, 5);    
+        //graph.draw(new Ellipse2D.Double(latTrasf, lonTrasf, 5, 5));
+        graph.drawOval(latTrasf.intValue(), lonTrasf.intValue(), 5, 5);
+        graph.fillOval(latitude.intValue(), longitude.intValue(), 5, 5);    
     }
 }
