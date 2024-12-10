2f6147ead75d7350ab3c98e2f248564958b0567c
ber-god <bernardowiz65@gmail.com>
2024-12-10 09:30:26 +0100

    Colored points created when clicked

--- src/main/java/com/_1/hex/DeliveryPlanning/view/DrawMap.java
+++ src/main/java/com/_1/hex/DeliveryPlanning/view/DrawMap.java
@@ -1,6 +1,5 @@
 package com._1.hex.DeliveryPlanning.view;
 
-
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -17,6 +16,7 @@ import javax.swing.JFrame;
 
 /**
  * This program demonstrates how to draw lines using Graphics2D object.
+ * 
  * @author www.codejava.net
  *
  */
@@ -37,41 +37,41 @@ public class DrawMap extends JFrame {
         setLocationRelativeTo(null);
     }
 
-    void defineStreetMap (StreetMap streetMap) {
+    void defineStreetMap(StreetMap streetMap) {
         this.streetMap = streetMap;
     }
 
-    void defineRoute (List<Integer> route) {this.route = route;}
-
-    Double normalizeLatitude(Double latitude){
-        return (latitude - minLatitudeValue) * 1000 /(maxLatitudeValue-minLatitudeValue);
+    void defineRoute(List<Integer> route) {
+        this.route = route;
     }
 
-    Double normalizeLongitude(Double longitude){
-        return (longitude - minLongitudeValue) * 1000 /(maxLongitudeValue-minLongitudeValue);
+    Double normalizeLatitude(Double latitude) {
+        return (latitude - minLatitudeValue) * 1000 / (maxLatitudeValue - minLatitudeValue);
     }
 
+    Double normalizeLongitude(Double longitude) {
+        return (longitude - minLongitudeValue) * 1000 / (maxLongitudeValue - minLongitudeValue);
+    }
 
     void intialise_MinAndMaxValues_For_LatitudeAndLongitude(Map<Integer, Intersection> intersections) {
         for (Integer key : intersections.keySet()) {
             Intersection intersection = intersections.get(key);
-            if(intersection.getLatitude()<minLatitudeValue){
+            if (intersection.getLatitude() < minLatitudeValue) {
                 minLatitudeValue = intersection.getLatitude();
             }
-            if(intersection.getLongitude()<minLongitudeValue){
+            if (intersection.getLongitude() < minLongitudeValue) {
                 minLongitudeValue = intersection.getLongitude();
             }
-            if(intersection.getLatitude()>maxLatitudeValue){
+            if (intersection.getLatitude() > maxLatitudeValue) {
                 maxLatitudeValue = intersection.getLatitude();
             }
-            if(intersection.getLongitude()>maxLongitudeValue){
+            if (intersection.getLongitude() > maxLongitudeValue) {
                 maxLongitudeValue = intersection.getLongitude();
             }
         }
     }
 
-
-    void drawStreetsOnJFrame_FromStreetMap (Graphics g) {
+    void drawStreetsOnJFrame_FromStreetMap(Graphics g) {
         Graphics2D g3d = (Graphics2D) g;
         g3d.setColor(Color.black);
 
@@ -87,21 +87,23 @@ public class DrawMap extends JFrame {
         }
     }
 
-
-    void drawPointsWhenIntersectionsIsClicked(){
+    void drawPointsWhenIntersectionsIsClicked() {
         List<Integer> clicksCounter = new ArrayList<>();
         clicksCounter.add(0);
 
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                
+                Integer iterator = 1;
+
                 for (Intersection intersection : streetMap.getIntersections().values()) {
-                    Ellipse2D.Double circle = new Ellipse2D.Double(normalizeLatitude(intersection.getLatitude()) - 5, normalizeLongitude(intersection.getLongitude()) - 5, 10, 10);
-                    
+                    Ellipse2D.Double circle = new Ellipse2D.Double(normalizeLatitude(intersection.getLatitude()) - 5,
+                            normalizeLongitude(intersection.getLongitude()) - 5, 10, 10);
+
                     if (circle.contains(e.getPoint())) {
                         if (clicksCounter.get(0) == 0) {
-                            Rectangle.Double rectangle = new Rectangle.Double(circle.x, circle.y, circle.width, circle.height);
+                            Rectangle.Double rectangle = new Rectangle.Double(circle.x, circle.y, circle.width,
+                                    circle.height);
                             Graphics g = getGraphics();
                             Graphics2D graph = (Graphics2D) g;
                             graph.setColor(Color.DARK_GRAY);
@@ -109,25 +111,28 @@ public class DrawMap extends JFrame {
                             clicksCounter.set(0, -1);
                             clicksCounter.add(0);
                         }
-                    
+
                         else {
-                            for (Integer i : clicksCounter)
-                            if (i == 0)
-                            {
-                                Rectangle.Double rectangle = new Rectangle.Double(circle.x, circle.y, circle.width, circle.height);
-                                Graphics g = getGraphics();
-                                Graphics2D graph = (Graphics2D) g;
-                                graph.setColor(Color.RED);
-                                graph.fill(rectangle);
-                                clicksCounter.set(i, 1);
-                            }
-                            else if (i == 1)
-                            {
-                                Graphics g = getGraphics();
-                                Graphics2D graph = (Graphics2D) g;
-                                graph.setColor(Color.RED);
-                                graph.fill(circle);clicksCounter.set(i, 2);
-                                clicksCounter.add(0);
+                            for (int i = iterator; i < clicksCounter.size(); i++) {
+                                if (clicksCounter.get(i) == 1) {
+                                    Graphics g = getGraphics();
+                                    Graphics2D graph = (Graphics2D) g;
+                                    graph.setColor(new Color((i * 50) % 256, (i * 80) % 256, (i * 110) % 256));
+                                    graph.fill(circle);
+                                    clicksCounter.set(i, 2);
+                                    clicksCounter.add(0);
+                                    iterator++;
+                                    break;
+                                } else if (clicksCounter.get(i) == 0) {
+                                    Rectangle.Double rectangle = new Rectangle.Double(circle.x, circle.y, circle.width,
+                                            circle.height);
+                                    Graphics g = getGraphics();
+                                    Graphics2D graph = (Graphics2D) g;
+                                    graph.setColor(new Color((i * 50) % 256, (i * 80) % 256, (i * 110) % 256));
+                                    graph.fill(rectangle);
+                                    clicksCounter.set(i, 1);
+                                    break;
+                                }
                             }
                         }
                     }
@@ -136,15 +141,14 @@ public class DrawMap extends JFrame {
         });
     }
 
-
-    void drawRoutes_FromTheOriginAndDestination (Graphics g) {
+    void drawRoutes_FromTheOriginAndDestination(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setColor(Color.GREEN);
         g2d.setStroke(new BasicStroke(2f));
         Double x1, y1, x2, y2;
-        for (int i=0; i<route.size()-1; i++){
+        for (int i = 0; i < route.size() - 1; i++) {
             Integer id1 = route.get(i);
-            Integer id2 = route.get(i+1);
+            Integer id2 = route.get(i + 1);
             Intersection source = streetMap.getIntersectionById(id1);
             Intersection destination = streetMap.getIntersectionById(id2);
             x1 = normalizeLatitude(source.getLatitude());
@@ -155,42 +159,45 @@ public class DrawMap extends JFrame {
         }
     }
 
-/*
-    void listenToClicksOnIntersections (Graphics g) {
-        
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                for (Intersection intersection : streetMap.getIntersections().values()) {
-                    Double latTrasf = normalizeLatitude(intersection.getLatitude());
-                    Double lonTrasf = normalizeLongitude(intersection.getLongitude());
-                    Ellipse2D.Double circle = new Ellipse2D.Double(latTrasf - 5.0, lonTrasf - 5.0, 10, 10);
-                    if (circle.contains(e.getPoint())) {
-                        System.out.println("Intersection clicked: Latitude: " + intersection.getLatitude() + ", Longitude: " + intersection.getLongitude());
-                        clickedIntersectionId[0] = intersection.getId();
-                        break;
-                    }
-                }
-            }
-        });
-    }
- */
+    /*
+     * void listenToClicksOnIntersections (Graphics g) {
+     * 
+     * addMouseListener(new MouseAdapter() {
+     * 
+     * @Override
+     * public void mouseClicked(MouseEvent e) {
+     * for (Intersection intersection : streetMap.getIntersections().values()) {
+     * Double latTrasf = normalizeLatitude(intersection.getLatitude());
+     * Double lonTrasf = normalizeLongitude(intersection.getLongitude());
+     * Ellipse2D.Double circle = new Ellipse2D.Double(latTrasf - 5.0, lonTrasf -
+     * 5.0, 10, 10);
+     * if (circle.contains(e.getPoint())) {
+     * System.out.println("Intersection clicked: Latitude: " +
+     * intersection.getLatitude() + ", Longitude: " + intersection.getLongitude());
+     * clickedIntersectionId[0] = intersection.getId();
+     * break;
+     * }
+     * }
+     * }
+     * });
+     * }
+     */
 
     public void paint(Graphics g) {
         super.paint(g);
 
         intialise_MinAndMaxValues_For_LatitudeAndLongitude(streetMap.getIntersections());
 
-        drawStreetsOnJFrame_FromStreetMap (g);
-        if(route != null) {
+        drawStreetsOnJFrame_FromStreetMap(g);
+        if (route != null) {
             drawRoutes_FromTheOriginAndDestination(g);
         }
 
         drawPointsWhenIntersectionsIsClicked();
 
-        //listenToClicksOnIntersections();
+        // listenToClicksOnIntersections();
 
-        //drawPoints(g, streetMap.getIntersectionById(route.get(2)));
+        // drawPoints(g, streetMap.getIntersectionById(route.get(2)));
     }
 
 }
